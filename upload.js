9b60332a1d1e4076fe9ecbc0ab559a19b54b1eb5
Santosh D <vebmate@gmail.com>
2015-10-14 12:45:22 +0530

    upload.js completed

--- upload.js
+++ upload.js
@@ -1,8 +1,10 @@
 var formy=document.getElementById('formy');
+
 formy.submit(function(e){
 e.preventDefault();
 var email=$('#email').val();
 var pass=$('#pass').val();
+check(email,pass);
 
 if(email=="vebmate@gmail.com" && pass=="hello")
 alert("Hurray...Your logged In !!");
@@ -10,3 +12,11 @@ else
 alert("Sorry...You have went somewhere wrong !!");
 });
 
+function check(getEmail,getPass)
+{
+if(getEmail.length<=6)
+alert("Invalid name");
+if(getPass.lenght<=6)
+alert("Invalid pass");
+
+}
