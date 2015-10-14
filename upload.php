dd4f36d33ca4b275f28010ba6c8b60179ac1e191
Santosh D <vebmate@gmail.com>
2015-10-14 10:56:57 +0530

    upload...php !!
    yoyoyo

---
+++ upload.php
@@ -0,0 +1,10 @@
+<?php
+
+$email=$_POST['email'];
+$pass=$_POST['pass'];
+
+if($email=="vebmate@gmail.com" && $pass=="hello")
+echo "Congo.., your logged in";
+else
+echo "Sorry...looks you have missed somewhere !!";
+?>
