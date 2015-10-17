847a8466bbfb661038a2085050eb169eacc4a1f0
vikasmb123 <vickybgoudar@gmail.com>
2015-10-16 19:03:03 -0700

    Changed Working

--- upload.js
+++ upload.js
@@ -1,22 +1,4 @@
-var formy=document.getElementById('formy');
-
-formy.submit(function(e){
-e.preventDefault();
-var email=$('#email').val();
-var pass=$('#pass').val();
-check(email,pass);
-
-if(email=="vebmate@gmail.com" && pass=="hello")
-alert("Hurray...Your logged In !!");
-else
-alert("Sorry...You have went somewhere wrong !!");
-});
-
-function check(getEmail,getPass)
+function check()
 {
-if(getEmail.length<=6)
-alert("Invalid name");
-if(getPass.lenght<=6)
-alert("Invalid pass");
-
+alert("vikas");
 }
